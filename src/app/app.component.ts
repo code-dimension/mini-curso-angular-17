28974a56ed6174c631f242571e8f08a0a1bc6b40
Henrique Custódia <martinshenrique8@gmail.com>
2023-11-08 19:04:31 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  template: `
+    <h1>Welcome to {{title}}!</h1>
+
+    <router-outlet></router-outlet>
+  `,
+  styles: [],
+})
+export class AppComponent {
+  title = 'ng-17-todo-list';
+}
