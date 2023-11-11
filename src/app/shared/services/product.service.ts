7cbe305feb566e149bae24ad685d2546d9921203
Henrique Custódia <martinshenrique8@gmail.com>
2023-11-11 11:59:22 -0300

    feat: criat productservice

---
+++ src/app/shared/services/product.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, inject } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Product } from "../interfaces/product.interface";
+
+@Injectable({
+  providedIn: "root",
+})
+export class ProductsService {
+  httpClient = inject(HttpClient);
+
+  getAll() {
+    return this.httpClient.get<Product[]>("/api/products");
+  }
+
+  getById(id: number) {
+    return this.httpClient.get<any>(`/api/products/${id}`);
+  }
+
+  post(payload: Exclude<Product, "id">) {
+    return this.httpClient.post<any>(`/api/products/`, payload);
+  }
+
+  patch(id: number, payload: Partial<Product>) {
+    return this.httpClient.patch<any>(`/api/products/${id}`, payload);
+  }
+
+  delete(id: number) {
+    return this.httpClient.delete<any>(`/api/products/${id}`);
+  }
+}
