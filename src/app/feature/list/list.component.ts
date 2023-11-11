db6a81912cc9470febcee74895cd022e2c260c2f
Henrique Custódia <martinshenrique8@gmail.com>
2023-11-11 12:00:26 -0300

    feat: cria ListComponent para listar os produtos

---
+++ src/app/feature/list/list.component.ts
@@ -0,0 +1,26 @@
+import { Component, inject } from "@angular/core";
+import { ProductsService } from "../../shared/services/product.service";
+import { AsyncPipe } from "@angular/common";
+import { CardComponent } from "./components/card/card.component";
+import { Product } from "../../shared/interfaces/product.interface";
+
+@Component({
+  selector: "app-list",
+  standalone: true,
+  imports: [AsyncPipe, CardComponent],
+  templateUrl: "./list.component.html",
+  styleUrl: "./list.component.scss",
+})
+export default class ListComponent {
+  productsService = inject(ProductsService);
+
+  products$ = this.productsService.getAll();
+
+  onDelete(taskId: number) {
+    this.productsService.delete(taskId);
+  }
+
+  onChange(product: Product) {
+    throw new Error("Method not implemented.");
+  }
+}
