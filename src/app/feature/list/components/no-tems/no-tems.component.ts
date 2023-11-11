a1bbf060c585b358441b8f716c2c2636284cc33e
Henrique Custódia <martinshenrique8@gmail.com>
2023-11-11 17:22:48 -0300

    feat: cria componente que é mostrado quando nenhum produto é encontrado

---
+++ src/app/feature/list/components/no-tems/no-tems.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-no-tems',
+  standalone: true,
+  imports: [MatCardModule],
+  templateUrl: './no-tems.component.html',
+  styleUrl: './no-tems.component.scss'
+})
+export class NoTemsComponent {
+
+}
