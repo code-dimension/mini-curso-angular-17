6ac31b98ad6069a87a6c581c2b506c046c41bebf
Henrique Custódia <martinshenrique8@gmail.com>
2023-11-12 20:39:50 -0300

    feat: cria componente especializado em editar produtos

---
+++ src/app/feature/edit/edit.component.ts
@@ -0,0 +1,31 @@
+import { Component, inject } from "@angular/core";
+import FormComponent from "../../shared/components/form/form.component";
+import { ProductsService } from "../../shared/services/product.service";
+import { ActivatedRoute, Router } from "@angular/router";
+import { ProductWithoutId } from "../../shared/interfaces/create-product.interface";
+import { Product } from "../../shared/interfaces/product.interface";
+
+@Component({
+  selector: "app-edit",
+  standalone: true,
+  imports: [FormComponent],
+  templateUrl: "./edit.component.html",
+  styleUrl: "./edit.component.scss",
+})
+export default class EditComponent {
+  private productsService = inject(ProductsService);
+  private router = inject(Router);
+  private activatedRoute = inject(ActivatedRoute);
+  
+  product: Product = this.activatedRoute.snapshot.data["product"];
+  
+  onSave(data: ProductWithoutId) {
+    this.save(data).subscribe(() => {
+      this.router.navigate(["/"]);
+    });
+  }
+
+  private save(payload: ProductWithoutId) {
+    return this.productsService.patch(this.product.id, payload);
+  }
+}
