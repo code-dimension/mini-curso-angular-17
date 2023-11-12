f8c1848d1aff3e482df8c08c026e5a58ca982ef3
Henrique Custódia <martinshenrique8@gmail.com>
2023-11-12 20:39:27 -0300

    feat: cria componente especializado em criar produtos

---
+++ src/app/feature/create/create.component.ts
@@ -0,0 +1,28 @@
+import { Component, inject } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ProductWithoutId } from "../../shared/interfaces/create-product.interface";
+import { Router } from "@angular/router";
+import { ProductsService } from "../../shared/services/product.service";
+import FormComponent from "../../shared/components/form/form.component";
+
+@Component({
+  selector: "app-create",
+  standalone: true,
+  imports: [FormComponent],
+  templateUrl: "./create.component.html",
+  styleUrl: "./create.component.scss",
+})
+export default class CreateComponent {
+  private productsService = inject(ProductsService);
+  private router = inject(Router);
+
+  onSave(data: ProductWithoutId) {
+    this.createProduct(data).subscribe(() => {
+      this.router.navigate(["/"]);
+    });
+  }
+
+  private createProduct(payload: ProductWithoutId) {
+    return this.productsService.post(payload);
+  }
+}
