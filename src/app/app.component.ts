12d90eec254f5bda2199bd271d38953e50cb767c
Henrique Custódia <martinshenrique8@gmail.com>
2024-02-04 18:14:06 -0300

    feat: adiciona header ao componente app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'product-store';
-}
+export class AppComponent { }
