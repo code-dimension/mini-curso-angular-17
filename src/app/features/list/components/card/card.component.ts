b7d7dd661bfd33bc1b98fe72c3ed3b6a6b99c2cf
Henrique Custódia <martinshenrique8@gmail.com>
2024-02-07 20:04:46 -0300

    refactor: refatora listagem de produto criando um componente para renderizar o mesmo

---
+++ src/app/features/list/components/card/card.component.ts
@@ -0,0 +1,17 @@
+import { Component, computed, input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { Product } from '../../../../shared/interfaces/product.interface';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [MatCardModule, MatButtonModule],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss',
+})
+export class CardComponent {
+  product = input.required<Product>();
+
+  productTitle = computed(() => this.product().title);
+}
--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -4,11 +4,12 @@ import { ProductsService } from '../../shared/services/products.service';
 import { Product } from '../../shared/interfaces/product.interface';
 import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
+import { CardComponent } from './components/card/card.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [CardComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
