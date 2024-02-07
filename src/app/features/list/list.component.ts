247eae54981a832e1b00272771f74da12867ab76
Henrique Custódia <martinshenrique8@gmail.com>
2024-02-07 20:23:05 -0300

    feat: adiciona link para criar um produto

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -5,11 +5,12 @@ import { Product } from '../../shared/interfaces/product.interface';
 import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
 import { CardComponent } from './components/card/card.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, RouterLink, MatButtonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
