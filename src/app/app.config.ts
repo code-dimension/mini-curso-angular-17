884dcc2e6aed1e9028597ebcf07ab197f09e71eb
Henrique Custódia <martinshenrique8@gmail.com>
2024-02-10 20:29:24 -0300

    refactor: define configuração da snackbar de forma global

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,28 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, ValueProvider } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
+import {
+  MAT_SNACK_BAR_DEFAULT_OPTIONS,
+  MatSnackBarConfig,
+} from '@angular/material/snack-bar';
+
+const SNACK_BAR_CONFIG: ValueProvider = {
+  provide: MAT_SNACK_BAR_DEFAULT_OPTIONS,
+  useValue: {
+    duration: 3000,
+    horizontalPosition: 'right',
+    verticalPosition: 'top',
+  } as MatSnackBarConfig,
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
     provideHttpClient(),
+    SNACK_BAR_CONFIG
   ],
 };
--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -42,11 +42,7 @@ export class CreateComponent {
         title: this.form.controls.title.value,
       })
       .subscribe(() => {
-        this.matSnackBar.open('Produto criado com sucesso!', 'Ok', {
-          duration: 3000,
-          horizontalPosition: 'right',
-          verticalPosition: 'top',
-        });
+        this.matSnackBar.open('Produto criado com sucesso!', 'Ok');
 
         this.router.navigateByUrl('/');
       });
