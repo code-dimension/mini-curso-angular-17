c15dc73a987e10fe7dd3d5607fd7adbc41e9220a
Henrique Custódia <martinshenrique8@gmail.com>
2024-02-11 22:42:17 -0300

    feat: utiliza o component back-to-list em edição e criação

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -1,14 +1,15 @@
 import { Component, inject } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ProductsService } from '../../shared/services/products.service';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { FormComponent } from '../../shared/components/form/form.component';
 import { Product } from '../../shared/interfaces/product.interface';
+import { BackToListComponent } from '../../shared/components/back-to-list/back-to-list.component';
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [FormComponent],
+  imports: [FormComponent, BackToListComponent],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss',
 })
--- src/app/features/edit/edit.component.ts
+++ src/app/features/edit/edit.component.ts
@@ -4,11 +4,12 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Product } from '../../shared/interfaces/product.interface';
 import { FormComponent } from '../../shared/components/form/form.component';
+import { BackToListComponent } from '../../shared/components/back-to-list/back-to-list.component';
 
 @Component({
   selector: 'app-edit',
   standalone: true,
-  imports: [FormComponent],
+  imports: [FormComponent, BackToListComponent],
   templateUrl: './edit.component.html',
   styleUrl: './edit.component.scss',
 })
