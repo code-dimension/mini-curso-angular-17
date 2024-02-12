0c7f9edd4ed90fb364a42c25c1a933899c67978d
Henrique Custódia <martinshenrique8@gmail.com>
2024-02-11 21:27:54 -0300

    refactor: melhora código de ListComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,6 +12,12 @@ import { ProductsService } from './shared/services/products.service';
 export const routes: Routes = [
   {
     path: '',
+    resolve: {
+      products: () => {
+        const productsService = inject(ProductsService)
+        return productsService.getAll();
+      }
+    },
     component: ListComponent,
   },
   {
--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,9 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { ProductsService } from '../../shared/services/products.service';
 import { Product } from '../../shared/interfaces/product.interface';
 import { MatButtonModule } from '@angular/material/button';
 import { CardComponent } from './components/card/card.component';
-import { Router, RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { filter } from 'rxjs';
 import { ConfirmationDialogService } from '../../shared/services/confirmation-dialog.service';
 
@@ -15,17 +15,13 @@ import { ConfirmationDialogService } from '../../shared/services/confirmation-di
   styleUrl: './list.component.scss',
 })
 export class ListComponent {
-  products: Product[] = [];
+  products = signal<Product[]>(
+    inject(ActivatedRoute).snapshot.data['products']
+  );
 
   productsService = inject(ProductsService);
   router = inject(Router);
   confirmationDialogService = inject(ConfirmationDialogService);
-
-  ngOnInit() {
-    this.productsService.getAll().subscribe((products) => {
-      this.products = products;
-    });
-  }
 
   onEdit(product: Product) {
     this.router.navigate(['/edit-product', product.id]);
@@ -38,7 +34,7 @@ export class ListComponent {
       .subscribe(() => {
         this.productsService.delete(product.id).subscribe(() => {
           this.productsService.getAll().subscribe((products) => {
-            this.products = products;
+            this.products.set(products);
           });
         });
       });
